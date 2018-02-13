6ec952145ca8772729429c36df2b6ada814c3142
Markus Fras <fras@mpp.mpg.de>
2018-02-13 09:27:36 +0100

    Software added to initialize a Silicon Labs clock generator / jitter attenuator chip (e.g. Si5338, Si5324) via the I2C port of the FTDI FT232H chip.

---
+++ Software/C/I2C/i2c-si5xxx-init/i2c-si5xxx-init.h
@@ -0,0 +1,29 @@
+// File: i2c-si5xxx-init.h
+// Auth: M. Fras, Electronics Division, MPI for Physics, Munich
+// Mod.: M. Fras, Electronics Division, MPI for Physics, Munich
+// Date: 12 Feb 2018
+// Rev.: 13 Feb 2018
+//
+// Header file for the initialization of a Silicon Labs clock generator /
+// jitter attenuator chip (e.g. Si5338, Si5324) via I2C.
+//
+
+
+
+#include "i2c_mpsse.h"
+
+
+
+// Message prefixes.
+#define PREFIX_DEBUG            "DEBUG: "
+#define PREFIX_ERROR            "ERROR: "
+
+
+
+// Level of debug info.
+#define DEBUG_LEVEL 0
+//#define DEBUG_LEVEL 1
+//#define DEBUG_LEVEL 2
+//#define DEBUG_LEVEL 3
+//#define DEBUG_LEVEL 4
+
